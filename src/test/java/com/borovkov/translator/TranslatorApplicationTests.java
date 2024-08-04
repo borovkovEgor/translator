b3db94148314ba0e18c72241e0f25f5a3351a7d3
borovkovEgor <borovkov.egor@yandex.ru>
2024-08-04 18:12:28 +0300

    добавил тест

--- src/test/java/com/borovkov/translator/TranslatorApplicationTests.java
+++ src/test/java/com/borovkov/translator/TranslatorApplicationTests.java
@@ -1,13 +1,24 @@
 package com.borovkov.translator;
 
+import com.borovkov.translator.service.TranslationService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TranslatorApplicationTests {
 
+    @Autowired
+    private TranslationService translationService;
     @Test
-    void contextLoads() {
+    public void translateTest() {
+        String sourceText = "Во всякой книге предисловие есть первая и вместе с тем последняя вещь; оно или служит объяснением цели сочинения, или оправданием и ответом на критики.";
+        String expectedTranslatedText = "In any book preface There is first And together With those last thing; it or serves explanation goals essays, or justification And answer on critics.";
+
+        String translatedText = translationService.translationText(sourceText, "ru", "en");
+
+        Assertions.assertEquals(translatedText, expectedTranslatedText);
     }
 
 }
